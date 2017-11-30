9189efb3316ed20e8ea647daaddb4178ff28e181
Matthias Hertel <me@matthiashertel.eu>
2017-11-30 09:17:22 +0100

    items are files in dir

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/manifoldco/promptui"
 )
 
+func getFiles() []string {
+	files, err := ioutil.ReadDir("./test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result []string
+	for _, f := range files {
+		result = append(result, f.Name())
+	}
+	return result
+}
+
 func main() {
 	prompt := promptui.Select{
-		Label: "Select Option",
-		Items: []string{"Opt1", "Opt2", "Opt3"},
+		Label: "Select Files",
+		Items: getFiles(),
 	}
 
 	_, result, err := prompt.Run()
